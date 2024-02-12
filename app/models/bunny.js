95a76b09b9c4c0c9a1e152e44928f2588e490dd6
albi <albijon.re@gmail.com>
2024-02-12 12:40:52 -0500

    routes for pets and added virtuals

--- app/models/bunny.js
+++ app/models/bunny.js
@@ -22,7 +22,23 @@ const bunnySchema = new mongoose.Schema(
 	},
 	{
 		timestamps: true,
+		toObject: {virtuals: true},
+		toJSON: {virtuals: true},
 	}
 )
+
+bunnySchema.virtual('fullTitle').get(function() {
+	return `${this.name} the ${this.type}`
+})
+
+bunnySchema.virtual('isABaby').get(function () {
+	if (this.age < 5) {
+		return "Yeah, theyre just a baby"
+	}else if (this.age >= 5 && this.age <10) {
+		return "not really a baby, but like, still a baby"
+	}else {
+		return "a good old bunny (definitely still a baby)"
+	}
+})
 
 module.exports = mongoose.model('Bunny', bunnySchema)
